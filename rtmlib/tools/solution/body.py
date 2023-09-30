14d6453927e96fe02d5e7f54a7332853dabebc8b
674106399@qq.com <674106399@qq.com>
2023-09-30 13:59:51 +0800

    add demo code

--- rtmlib/tools/solution/body.py
+++ rtmlib/tools/solution/body.py
@@ -1,3 +1,45 @@
+'''
+Example:
+
+import cv2
+
+from rtmlib import Body, draw_skeleton
+
+device = 'cuda'
+backend = 'onnxruntime'  # opencv, onnxruntime
+
+cap = cv2.VideoCapture('./demo.mp4')
+
+openpose_skeleton = True  # True for openpose-style, False for mmpose-style
+
+body = Body(to_openpose=openpose_skeleton,
+                      backend=backend,
+                      device=device)
+
+frame_idx = 0
+
+while cap.isOpened():
+    success, frame = cap.read()
+    frame_idx += 1
+
+    if not success:
+        break
+
+    keypoints, scores = body(frame)
+
+    img_show = frame.copy()
+
+    img_show = draw_skeleton(img_show,
+                             keypoints,
+                             scores,
+                             openpose_skeleton=openpose_skeleton,
+                             kpt_thr=0.43)
+
+    img_show = cv2.resize(img_show, (960, 540))
+    cv2.imshow('img', img_show)
+    cv2.waitKey(10)
+
+'''
 import numpy as np
 
 from .. import YOLOX, RTMPose
