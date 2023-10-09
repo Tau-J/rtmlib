6ad9e22b74ce9661f6f244436e7dff2e08111e10
Tau-J <674106399@qq.com>
2023-10-09 11:10:46 +0800

    mod python requires

--- setup.py
+++ setup.py
@@ -101,22 +101,21 @@ def parse_requirements(fname='requirements.txt', with_version=True):
 
 
 if __name__ == '__main__':
-    setup(
-        name='rtmlib',
-        version=get_version(),
-        description='A library for real-time pose estimation.',
-        author='Tau-J',
-        author_email='taujiang@outlook.com',
-        keywords='pose estimation',
-        long_description=readme(),
-        long_description_content_type='text/markdown',
-        packages=find_packages(),
-        include_package_data=True,
-        url='https://github.com/Tau-J/rtmlib',
-        license='Apache License 2.0',
-        python_requires='>=3.8',
-        install_requires=parse_requirements('requirements/runtime.txt'),
-        extras_require={
-            'all': parse_requirements('requirements.txt'),
-            'optional': parse_requirements('requirements/optional.txt'),
-        })
+    setup(name='rtmlib',
+          version=get_version(),
+          description='A library for real-time pose estimation.',
+          author='Tau-J',
+          author_email='taujiang@outlook.com',
+          keywords='pose estimation',
+          long_description=readme(),
+          long_description_content_type='text/markdown',
+          packages=find_packages(),
+          include_package_data=True,
+          url='https://github.com/Tau-J/rtmlib',
+          license='Apache License 2.0',
+          python_requires='>=3.7',
+          install_requires=parse_requirements('requirements/runtime.txt'),
+          extras_require={
+              'all': parse_requirements('requirements.txt'),
+              'optional': parse_requirements('requirements/optional.txt'),
+          })
