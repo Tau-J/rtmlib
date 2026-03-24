41a693d708aac812df38eafd1f3722946a9da90f
Saif Khan <saifkhichi96@gmail.com>
2026-03-24 09:27:05 +0100

    add RFDETR object detection tool and update imports

--- rtmlib/tools/__init__.py
+++ rtmlib/tools/__init__.py
@@ -1,10 +1,10 @@
-from .object_detection import YOLOX, RTMDet
+from .object_detection import YOLOX, RTMDet, RFDETR
 from .pose_estimation import RTMO, RTMPose, RTMPose3d, ViTPose
 from .solution import (Animal, Body, BodyWithFeet, Custom, Hand, PoseTracker,
                        Wholebody, Wholebody3d)
 
 __all__ = [
-    'YOLOX', 'RTMDet', 'RTMPose', 'RTMO', 'RTMPose3d', 'ViTPose',
+    'YOLOX', 'RTMDet', 'RFDETR', 'RTMPose', 'RTMO', 'RTMPose3d', 'ViTPose',
     'PoseTracker', 'Wholebody', 'Wholebody3d', 'Body', 'Hand', 'BodyWithFeet',
     'Animal', 'Custom'
 ]
